3b6ee05ce7e4d068ae06c6c2870c994babdde743
Fidelis Okeke <padrefidelis111@gmail.com>
2022-10-18 08:03:12 +0100

    made small changes

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -95,34 +95,6 @@ export default function WorkExperience() {
                 </div>
               </div>
             </div>
-
-            <div className="timeline-item mt-12">
-              <div className="grid sm:grid-cols-2">
-                <div className="md:order-1 order-2">
-                  <div className="event event-description-left float-left text-right md:mr-8">
-                    <h5 className="title mb-1 font-semibold">
-                      Front-end Web Developer
-                    </h5>
-                    <p className="timeline-subtitle mt-3 mb-0 text-slate-400">
-                      The generated injected humour, or non-characteristic words
-                      etc. Cum sociis natoque penatibus et magnis dis parturient
-                      montes, nascetur ridiculus mus. Donec quam felis,
-                    </p>
-                  </div>
-                </div>
-                <div className="md:order-2 order-1">
-                  <div className="duration duration-right md:ml-8 relative">
-                    <img
-                      src="assets/images/client/circle-logo.png"
-                      className="rounded-full h-9 w-9"
-                      alt=""
-                    />
-                    <h5 className="my-2 font-semibold">Circle CI</h5>
-                    <h6 className="text-slate text-sm mb-0">2018-19</h6>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
